2379e7da95822e615678306be39bc456bb996c9b
Yousef Shaban <yousefshaban.eg@gmail.com>
2025-07-12 05:54:46 +0300

    Refactor environment variable validation in server startup process to improve readability and maintainability.

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -7,23 +7,22 @@ import { ExpirationCompleteListener } from "./events/listeners/ExpirationComplet
 import { PaymentCreatedListener } from "./events/listeners/PaymentCreatedListener";
 
 const start = async () => {
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI must be defined");
+  }
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be defined");
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error("NATS_CLUSTER_ID must be defined");
+  }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("NATS_CLIENT_ID must be defined");
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error("NATS_URL must be defined");
+  }
   try {
-    if (!process.env.MONGO_URI) {
-      throw new Error("MONGO_URI must be defined");
-    }
-    if (!process.env.JWT_KEY) {
-      throw new Error("JWT_KEY must be defined");
-    }
-    if (!process.env.NATS_CLUSTER_ID) {
-      throw new Error("NATS_CLUSTER_ID must be defined");
-    }
-    if (!process.env.NATS_CLIENT_ID) {
-      throw new Error("NATS_CLIENT_ID must be defined");
-    }
-    if (!process.env.NATS_URL) {
-      throw new Error("NATS_URL must be defined");
-    }
-
     await natsWrapper.connect(
       process.env.NATS_CLUSTER_ID,
       process.env.NATS_CLIENT_ID,
