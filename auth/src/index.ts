f139ae34c57ee7ee1fa746dd2829d19b38e464da
Yousef Shaban <yousefshaban.eg@gmail.com>
2025-07-13 09:01:28 +0300

    Update error messages in auth module to remove exclamation marks for consistency

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,10 +3,10 @@ import { app } from "./app";
 
 const start = async () => {
   if (!process.env.MONGO_URI) {
-    throw new Error("MONGO_URI must be defined!");
+    throw new Error("MONGO_URI must be defined");
   }
   if (!process.env.JWT_KEY) {
-    throw new Error("JWT_KEY must be defined!");
+    throw new Error("JWT_KEY must be defined");
   }
 
   try {
