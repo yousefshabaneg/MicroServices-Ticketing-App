9c20cb989c75a05588a76d6917cc3f946e66836b
Yousef Shaban <yousefshaban.eg@gmail.com>
2025-07-13 10:24:06 +0300

    Refactor error messages in auth and expiration modules for consistency; update console log formatting in orders, payments, and tickets modules.

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -45,7 +45,7 @@ const start = async () => {
   const PORT = process.env.PORT || 3000;
 
   app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port: ${PORT}`);
   });
 };
 
